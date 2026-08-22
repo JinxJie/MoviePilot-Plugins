3f5b44dbf1115edee949e66bc8fe4b2868073b1c
JinxJie <244017248@qq.com>
2026-08-22 15:12:55 +0800

    chore: bump plugin version to 1.0.6

--- plugins/hhlottery/__init__.py
+++ plugins/hhlottery/__init__.py
@@ -33,7 +33,7 @@ class HHLottery(_PluginBase):
     plugin_name = "HHCLUB 自动抽奖"
     plugin_desc = "HHCLUB 自动抽奖增强版 · 大奖即时通知、站内信自动清理、Cron 定时运行 · 奖品名称汇总、今日/昨日分布 · 或者使用我的油猴脚本：HHCLUB 自动抽奖 · 庆典版 https://greasyfork.org/zh-CN/scripts/591722"
     plugin_icon = "hhlottery.png"
-    plugin_version = "1.0.5"
+    plugin_version = "1.0.6"
     plugin_author = "JinxJie"
     author_url = "https://github.com/JinxJie"
     plugin_config_prefix = "hhlottery_"
